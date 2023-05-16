e8fad765360ac88aac0989ebb76e8f6e67e21b06
vivi2005 <128539919+vivi2005@users.noreply.github.com>
2023-05-16 16:28:59 -0500

    Add files via upload

---
+++ ejercicio 1.py
@@ -0,0 +1,22 @@
+print ('mi primer ejercicio')
+# se utiliza la funcion print para generar un mensaje por consola 
+"""
+bloque de codigo grande 
+"""
+
+
+
+
+
+# metodo para concatenar cadenas de texto 
+ 
+nombre = 'Yurany Restrepo Gutierrez'
+edad = 21
+cadena_1 = 'mi nombre es:'
+cadena_2 = 'mi edad es:'
+
+# imprimir por pantalla 
+print(cadena_1, nombre, cadena_2, edad)
+
+print (f'Nombre:{nombre}\n edad: {edad}')
+
